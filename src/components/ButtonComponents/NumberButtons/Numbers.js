4f870102982e55fba380c0273d7f9d265c55c5fe
adamwinzdesign <adamwinzdesign@gmail.com>
2019-10-09 15:09:54 -0500

    borked

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -7,13 +7,22 @@ import { numbers } from '../../../data'
 */
 //Import your array data to from the provided data file
 
-const Numbers = () => {
+import {numbers} from "../../../data";
+
+export const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [numberState, setNumberState] = useState(numbers)
+
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/
+       }
+       <div className = 'button'>
+        {numberState.map((currentNumber, index) => <NumberButton key = {index} currentNumber = {currentNumber} currentIndex = {index} />)}
+       </div>
+
     </div>
   );
 };
